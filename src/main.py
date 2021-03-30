7a0be89c8900bc3543ec696cb9080976beab6d03
Hittokirizero <v_hbk@hotmail.com>
2021-03-30 03:38:07 +0000

    Get,Post and Delete

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todolist
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@app.route('/get_task', methods=['GET'])
+def get_task_td():
+    gt_task = Todolist.query.all()
+    all_task = list(map(lambda x: x.serialize(), gt_task))
+    return jsonify(all_task), 200
+
+@app.route('/post_task', methods=['POST'])
+def post_task_td():
+    request_body = request.get_json()
+    insert_task = Todolist(td_task=request_body["label"], is_done=request_body["done"])
+    db.session.add(insert_task)
+    db.session.commit()
+    return jsonify({"Todo ok" : request_body }), 200
+
+
+@app.route('/del_task/<int:numb>', methods=['DELETE'])
+def del_task_td(numb):
+    task = Todolist.query.filter_by(id = numb).first()
+    if task is None:
+        raise APIException('Lo que buscas no existe', status_code=404)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify({"Elementos elimando numero de ID:": numb}), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,6 +2,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class Todolist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    td_task = db.Column(db.String(120), unique=False, nullable=False)
+    is_done = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<Todolist %r>' % self.td_task
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "label": self.td_task,
+            "done": self.is_done
+        }
+
+
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
